a001f0e2e948d8fae6f350dc3b0ebddc5d93f5e5
Emilio Coppola <coppolaemilio@gmail.com>
2019-03-10 19:58:21 +0100

    part-2

--- Main.gd
+++ Main.gd
@@ -23,3 +23,21 @@ func _on_Button_pressed():
 
 func _on_GraphEdit_connection_request(from, from_slot, to, to_slot):
 	$GraphEdit.connect_node(from, from_slot, to, to_slot)
+
+
+func _on_GraphEdit_disconnection_request(from, from_slot, to, to_slot):
+	$GraphEdit.disconnect_node(from, from_slot, to, to_slot)
+
+
+func _on_RunProgram_pressed():
+	var G = $GraphEdit
+	var connection_list = G.get_connection_list()
+	print(G.get_connection_list())
+	var result = 0
+	for i in range(0, connection_list.size()):
+		var value = G.get_node(connection_list[i].from).get_node('SpinBox').value
+		var value_2 = G.get_node(connection_list[i].to).get_node('SpinBox').value
+		result += value
+		if i+1 == connection_list.size():
+			result += value_2
+	print(result)
